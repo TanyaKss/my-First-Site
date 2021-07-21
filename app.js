8a0047897d5d8a34f1ee4b5d69d0badd3e7ca3d6
Киселева Татьяна Васильевна <tkiseleva@tsc.ts>
2021-07-21 15:06:16 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+//Примитивные типы данных
+console.log(2019, 1.5);
+console.log(20 * 'bla bla');
+console.log (1/0);
+
+console.log('Hello', "World", `Boo`);
+
+console.log(true, false);
+
+console.log(null);
+
+console.log(undefined);
+
+console.log(Symbol());
+
+//Ссылочный тип данных - Объекты
+
+console.log({name:'Tanya', age: 25});
+console.log([1, 2, 3, 4]);
